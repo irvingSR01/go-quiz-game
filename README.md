acb619af7291b494469fd1843f3e610e76fb0a84
Irving Sanchez <irvinhirvi@gmail.com>
2025-04-08 11:44:39 -0600

    User can answer the questions, but need some fix yet

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ This project is created to learn some basics about Go programming language.
 3. timers
 4. csv files  
 
+# Details
+
+- [x] Quiz will be provided from csv file.
+- [ ] User should be able to change the default name "problems.csv" from flags.
+- [x] Format for cvs file should be question,answer.
+- [x] At the end of quiz, output need to display total of numbers of questions correct and total of questions.
+
+> ℹ️ __"what 2+2, sir?",4"__ should be a valid input from csv file.
+
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	results := make([]string, 0)
+	questions := make([]string, 0)
+	file, err := os.Open("problems.csv")
+	check(err)
+	defer file.Close()
+
+	cReader := bufio.NewReader(os.Stdin)
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	check(err)
+
+	for _, row := range records {
+		for j, cell := range row {
+			if j == 0 {
+				questions = append(questions, cell)
+			} else {
+				// estos resultados los tengo que guardar
+				results = append(results, cell)
+			}
+		}
+	}
+
+	total := len(results)
+	correct := 0
+	for i, v := range results {
+
+		fmt.Printf("%s =\n", questions[i])
+		text, err := cReader.ReadString('\n')
+		check(err)
+		text = strings.TrimSpace(text)
+		if text == v {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correct, total)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
